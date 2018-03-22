aec3bbf260aa18b41dba05b590ed9ed4297e8933
Jamie Nadel <jamienadel4@gmail.com>
2018-03-21 21:47:11 -0700

    added second question

--- js/app.js
+++ js/app.js
@@ -1,5 +1,7 @@
 'use strict';
 
+// user name
+
 var userName = prompt('What\'s your name?');
 
 if(!userName) {
@@ -11,14 +13,28 @@ console.log('USERNAME', userName);
 
 alert('Hello ' + userName + ' welcome to my about me page!'); 
 
-var metResponse = prompt('Have we met before?');
+// Question 1
 
-if(metResponse === 'yes') {
+var metResponse = prompt('Have we met before?').toLowerCase();
+
+if(metResponse === 'yes' || metResponse === 'y') {
   alert('I don\'t doubt it ' + userName + '!');
 }
 console.log('Have we met?', metResponse);
 
-if(metResponse === 'no') {
+if(metResponse === 'no' || metResponse === 'n') {
   alert('Wait why you on my site then? ' + userName + '?');
 } 
 
+// Question 2
+
+var doyoulikemystyleResponse = prompt('Do you like my style?').toLowerCase();
+
+if(doyoulikemystyleResponse === 'yes' || doyoulikemystyleResponse === "y") {
+  alert('I like your style too' + userName);
+}
+
+if(doyoulikemystyleResponse === 'no' || doyoulikemystyleResponse === "n") {
+  alert('Sashay away...');
+}
+console.log('Do you like my style?', doyoulikemystyleResponse);
