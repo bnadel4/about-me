1909e01eb2bdf9f889dd0998a79f18f7c2b30089
Jamie Nadel <jamienadel4@gmail.com>
2018-03-21 21:22:30 -0700

    added ifs

--- js/app.js
+++ js/app.js
@@ -2,8 +2,23 @@
 
 var userName = prompt('What\'s your name?');
 
-console.log(userName);
+if(!userName) {
+  alert('wuts ur name br0');
+  userName = prompt('What\'s your name?');
+}
 
-alert('Hello ' + userName + ' welcome to this');
+console.log('USERNAME', userName);
 
+alert('Hello ' + userName + ' welcome to my about me page!'); 
+
+var metResponse = prompt('Have we met before?');
+
+if(metResponse === 'yes') {
+  alert('I don\'t doubt it ' + userName + '!');
+}
+console.log('Have we met?', metResponse);
+
+if(metResponse === 'no') {
+  alert('Wait why you on my site then? ' + userName + '?');
+} 
 
