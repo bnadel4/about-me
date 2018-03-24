e259f3d54674b23f46eeff96d260c50d82bbc9d0
Jamie Nadel <jamienadel4@gmail.com>
2018-03-23 19:00:22 -0700

    all five questions added

--- js/app.js
+++ js/app.js
@@ -30,11 +30,56 @@ if(metResponse === 'no' || metResponse === 'n') {
 
 var doyoulikemystyleResponse = prompt('Do you like my style?').toLowerCase();
 
-if(doyoulikemystyleResponse === 'yes' || doyoulikemystyleResponse === "y") {
-  alert('I like your style too' + userName);
+if(doyoulikemystyleResponse === 'yes' || doyoulikemystyleResponse === 'y') {
+  alert('I like your style too ' + userName);
 }
 
-if(doyoulikemystyleResponse === 'no' || doyoulikemystyleResponse === "n") {
+if(doyoulikemystyleResponse === 'no' || doyoulikemystyleResponse === 'n') {
   alert('Sashay away...');
 }
 console.log('Do you like my style?', doyoulikemystyleResponse);
+
+// Question 3
+
+var antmResponse = prompt('Do you have what it takes to becomes americas next top model?').toLowerCase();
+
+if(antmResponse === 'yes'|| antmResponse ==='y') {
+  alert('haha bullshit ' + userName + '!');
+}
+
+if(antmResponse === 'no' || antmResponse === 'n') {
+  alert('I believe in you ' + userName + '!');
+}
+
+console.log('Do you have what it takes to become americas next top model?', antmResponse);
+
+// Question 4
+
+var hairsprayResponse = prompt('Have you seen my hairspray babe?').toLowerCase();
+
+if (hairsprayResponse === 'yes' || hairsprayResponse === 'y') {
+  alert('Hmmmm I dont see it ' + userName + ' let me use yours babe.');
+}
+
+if (hairsprayResponse === 'no' || hairsprayResponse === 'n') {
+  alert('Okay ' + userName + ' let me just use yours babe.');
+}
+
+console.log('Have you seen my hairspray babe?', hairsprayResponse);
+
+//Question 5
+
+var hairlookgoodResponse = prompt('Does my hair look good now?').toLowerCase();
+
+if (hairlookgoodResponse === 'yes' || hairlookgoodResponse === 'y') {
+  alert('Yeah right babe I look like a 90s Ben Stiller clone');
+}
+
+
+if (hairlookgoodResponse === 'no' || hairsprayResponse === 'n') {
+  alert('Babe this hairspray has no hold!');
+}
+
+console.log('Does my hair look good now?', hairlookgoodResponse);
+// this hairspray has no hold
+// i look like crap
